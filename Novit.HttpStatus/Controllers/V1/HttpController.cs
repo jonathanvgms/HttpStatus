8da601fa2d83ceefaa91de1de45d9d6255d68170
Jonathan Velazquez <jonathanvgms@gmail.com>
2021-11-15 19:30:29 -0300

    Add project

---
+++ Novit.HttpStatus/Controllers/V1/HttpController.cs
@@ -0,0 +1,54 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Novit.HttpStatus.Controllers.ViewModels;
+using System.Collections.Generic;
+
+namespace Novit.HttpStatus.Controllers.V1
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class HttpController : ControllerBase
+    {
+        [HttpGet]
+        public ActionResult<List<HttpRequestViewModel>> Get()
+        {
+            return Ok(new List<HttpRequestViewModel>() { new HttpRequestViewModel() {
+                codigo = 200,
+                nombre = "OK",
+                categoria = "Success",
+                referencia = "https://httpstatuses.com/200"
+            } });
+        }
+
+        [HttpGet("{codigo}")]
+        public ActionResult<HttpRequestViewModel> Get(int codigo)
+        {
+            return Ok();
+        }
+
+        [HttpPost]
+        public ActionResult Post([FromBody] HttpRequestViewModel http)
+        {
+            return Ok();
+        }
+
+        [HttpPost]
+        [Route("[action]")]
+        public ActionResult CrearConLista([FromBody] List<HttpRequestViewModel> https)
+        {
+            return Ok();
+        }
+
+        [HttpPut("{codigo}")]
+        public ActionResult Put(int codigo, HttpRequestViewModel http)
+        {
+            return Ok();
+        }
+
+        [HttpDelete("{codigo}")]
+        public ActionResult Delete(int codigo)
+        {
+            return Ok();
+        }
+    }
+}
---
+++ Novit.HttpStatus/Controllers/V1/ViewModels/HttpViewModel.cs
@@ -0,0 +1,11 @@
+﻿namespace Novit.HttpStatus.Controllers.V1.ViewModels
+{
+    public class HttpRequestViewModel
+    {
+        public int codigo { get; set; }
+        public string nombre { get; set; }
+        public string categoria { get; set; }
+        public string referencia { get; set; } //URL hacia httpstatus.com
+        public string imagen { get; set; }
+    }
+}
